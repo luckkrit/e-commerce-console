e31d791502edfd5f60ac5c2d977fe8ef5a4acbcf
Krit Chomaitong <luckkrit@hotmail.com>
2021-04-28 14:35:23 +0700

    Fix bug in addProduct. Add getProduct for get single product. Add getProductString for get string of product collection.

--- main/java/com/k9/ecommerce/product/ProductService.java
+++ main/java/com/k9/ecommerce/product/ProductService.java
@@ -1,25 +1,29 @@
 package com.k9.ecommerce.product;
 
 import javax.inject.Inject;
+import javax.inject.Singleton;
 import java.util.List;
 import java.util.Optional;
 
+@Singleton
 public class ProductService {
     @Inject
     public void setProductRepository(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
+    public ProductRepository getProductRepository() {
+        return productRepository;
+    }
+
     private ProductRepository productRepository;
 
     public void addProduct(Product product) {
-        long maxProductId = productRepository.getProducts().stream().mapToLong(Product::getId).max().orElse(1);
         if (product.getId() == 0) {
-            product.setId(maxProductId);
-        } else {
-            product.setId(maxProductId);
+            long maxProductId = productRepository.getProducts().stream().mapToLong(Product::getId).max().orElse(0);
+            product.setId(maxProductId + 1);
+            productRepository.addProduct(product);
         }
-        productRepository.addProduct(product);
     }
 
     public void removeProduct(long productId) {
@@ -29,6 +33,19 @@ public class ProductService {
 
     public List<Product> getProducts() {
         return this.productRepository.getProducts();
+    }
+
+    public Product getProduct(long productId) {
+        return this.productRepository.getProducts().stream().filter(p -> p.getId() == productId).findFirst().orElse(null);
+    }
+
+    public String getProductsString() {
+        StringBuilder sb = new StringBuilder();
+        for (var product : productRepository.getProducts()) {
+            sb.append(product.toString());
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 
     @Inject
