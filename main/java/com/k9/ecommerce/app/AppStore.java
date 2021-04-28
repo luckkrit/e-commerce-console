20057b2f98d0d1291ce641c08c6e0a164e5bfe9c
Krit Chomaitong <luckkrit@hotmail.com>
2021-04-28 14:15:44 +0700

    Remove dependency from AppStore

--- main/java/com/k9/ecommerce/app/AppStore.java
+++ main/java/com/k9/ecommerce/app/AppStore.java
@@ -1,49 +1,26 @@
 package com.k9.ecommerce.app;
 
-import com.k9.ecommerce.cart.CartService;
 import com.k9.ecommerce.menu.MainMenu;
-import com.k9.ecommerce.menu.Menu;
-import com.k9.ecommerce.product.ProductService;
+import com.k9.ecommerce.menu.MenuNavigator;
 
 import javax.inject.Inject;
 
 public class AppStore {
 
     public AppStore() {
-        setMenu(new MainMenu());
     }
+
+    MenuNavigator menuNavigator;
 
     @Inject
-    public void setProductService(ProductService productService) {
-        this.productService = productService;
-    }
-
-    public ProductService getProductService() {
-        return productService;
-    }
-
-    public CartService getCartService() {
-        return cartService;
-    }
-
-    private ProductService productService;
-
-    @Inject
-    public void setCartService(CartService cartService) {
-        this.cartService = cartService;
-    }
-
-    private CartService cartService;
-
-    public void setMenu(Menu menu) {
-        this.menu = menu;
+    public void setMenuNavigator(MenuNavigator menuNavigator) {
+        this.menuNavigator = menuNavigator;
     }
 
     public void showMenu() {
-        this.menu.showMenu(this);
+        if (this.menuNavigator.getMenu() == null) {
+            this.menuNavigator.setMenu(new MainMenu());
+        }
+        this.menuNavigator.showMenu();
     }
-
-    private Menu menu;
-
-
 }
